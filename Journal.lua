85d122510b2e78df7cc68834d8ab5af16eef2f0f
sfmict <sfmict@mail.ru>
2018-05-14 07:18:16 +0300

    update buttons

--- Journal.lua
+++ Journal.lua
@@ -49,6 +49,8 @@ function journal:ADDON_LOADED(addonName)
 		-- BUTTONS
 		journal.buttons = {MountJournalListScrollFrameScrollChild:GetChildren()}
 
+		print(journal.buttons[1].background:GetAtlas(), GetAtlasInfo(journal.buttons[1].background:GetAtlas()))
+
 		local function CreateButton(name, parent, pointX, pointY, bgTex, OnClick)
 			local btnFrame = CreateFrame("button", nil, parent)
 			btnFrame:SetPoint("TOPRIGHT", pointX, pointY)
@@ -56,21 +58,26 @@ function journal:ADDON_LOADED(addonName)
 			btnFrame:SetScript("OnClick", OnClick)
 			parent[name] = btnFrame
 
+			btnFrame:SetNormalTexture("Interface/AddOns/MountsJournal/textures/button.blp")
+			btnFrame:SetHighlightTexture("Interface/AddOns/MountsJournal/textures/button.blp")
+			local background, hightlight = btnFrame:GetRegions()
+			background:SetTexCoord(0.00390625, 0.8203125, 0.00390625, 0.18359375)
+			print(background:GetPoint())
+			-- background:SetVertexColor(0.1,0.08,0.01)
+			background:SetVertexColor(0.2,0.18,0.01)
+			hightlight:SetTexCoord(0.00390625, 0.8203125, 0.19140625, 0.37109375)
+
+			btnFrame.check = btnFrame:CreateTexture(nil, "ARTWORK")
+			btnFrame.check:SetTexture("Interface/AddOns/MountsJournal/textures/button.blp")
+			btnFrame.check:SetTexCoord(0.00390625, 0.8203125, 0.37890625, 0.55859375)
+			btnFrame.check:SetVertexColor(0.8,0.6,0)
+			btnFrame.check:SetAllPoints()
+
 			btnFrame.icon = btnFrame:CreateTexture(nil, "OVERLAY")
 			btnFrame.icon:SetTexture(bgTex)
 			btnFrame.icon:SetAllPoints()
-
-			btnFrame.check = btnFrame:CreateTexture(nil, "OVERLAY")
-			btnFrame.check:SetTexture("Interface/AddOns/MountsJournal/textures/button.blp")
-			btnFrame.check:SetTexCoord(0.00390625, 0.8203125, 0.37890625, 0.55859375)
-			btnFrame.check:SetAllPoints()
-			
-			btnFrame:SetNormalTexture("Interface/AddOns/MountsJournal/textures/button.blp")
-			btnFrame:SetHighlightTexture("Interface/AddOns/MountsJournal/textures/button.blp")
-			local _, _, background, hightlight = btnFrame:GetRegions()
-			background:SetTexCoord(0.00390625, 0.8203125, 0.00390625, 0.18359375)
-			background:SetVertexColor(0.1,0.05,0)
-			hightlight:SetTexCoord(0.00390625, 0.8203125, 0.19140625, 0.37109375)
+			-- btnFrame.icon:SetPoint("TOPLEFT", btnFrame, "TOPLEFT", 1,-1)
+			-- btnFrame.icon:SetSize(23, 11)
 
 			-- btnFrame:SetNormalTexture(bgTex)
 			-- btnFrame:SetHighligatTexture("Interface/Buttons/CheckButtonHilight-Blue")
