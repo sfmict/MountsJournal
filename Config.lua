c26fcb00687b87eccbc746b9d820e9f975d92f8e
sfmict <sfmict@mail.ru>
2018-07-16 09:19:02 +0300

    Revert "binding"
    This reverts commit d03f050e51581ee997aae23ea863e8b0282139c7.

--- Config.lua
+++ Config.lua
@@ -1,12 +1,7 @@
 local addon, L = ...
 local mounts = MountsJournal
-local binding = _G[addon.."Binding"]
 local config = CreateFrame("Frame", "MountsJournalConfig", InterfaceOptionsFramePanelContainer)
 config.name = addon
-
-
--- BIND MOUNT
-local bindMount = binding:createButtonBinding(config, "MountsJournal_Mount", "/mount")
 
 
 config:SetScript("OnShow", function()
@@ -111,9 +106,6 @@ config:SetScript("OnShow", function()
 
 	setTooltip(createMacroBtn, "ANCHOR_TOP", L["CreateMacro"], L["CreateMacroTooltip"])
 
-	-- BIND MOUNT
-	bindMount:SetPoint("TOPLEFT", createMacroBtn, "BOTTOMLEFT", 0, -20)
-
 	-- WATER WALKER EYE
 	local waterWalkerEye = CreateFrame("CheckButton", "MountsJournalWaterWalkEye", config, "InterfaceOptionsCheckButtonTemplate")
 	waterWalkerEye:SetPoint("LEFT", modifierCombobox, "RIGHT", 180, 2)
@@ -142,9 +134,6 @@ config:SetScript("OnShow", function()
 		waterJump:SetChecked(mounts.config.waterJump)
 		waterWalkerEye:SetChecked(mounts.config.waterWalkInstance)
 		waterWalkerAlways:SetChecked(mounts.config.waterWalkAll)
-		bindMount.oldKey = false
-		local key1 = GetBindingKey(bindMount.command, binding.mode)
-		bindMount.Text:SetText(key1 and GetBindingText(key1) or "")
 	end
 
 	config:SetScript("OnShow", refresh)
@@ -159,13 +148,6 @@ config.okay = function()
 	mounts.config.waterWalkAll = MountsJournalWaterWalkAlways:GetChecked()
 end
 
-
-config.cancel = function()
-	if bindMount.oldKey ~= false then
-		binding:setBinding(bindMount.oldKey, bindMount.command)
-		bindMount.oldKey = false
-	end
-end
 
 -- ADD CATEGORY
 InterfaceOptions_AddCategory(config)
