eade0c9e4e5e78b9e5e53ba93e389015025b02a4
sfmict <sfmict@mail.ru>
2018-08-08 17:39:52 +0300

    update binding

--- Binding.lua
+++ Binding.lua
@@ -73,96 +73,15 @@ function binding:OnKeyDown(keyPressed)
 	if GetBindingFromClick(keyPressed) == "SCREENSHOT" then
 		Screenshot()
 	elseif self.selected then
-		if keyPressed == "UNKNOWN" then return end
+		keyPressed = GetConvertedKeyOrButton(keyPressed)
 
-		if keyPressed == "LeftButton" then
-			keyPressed = "BUTTON1"
-		elseif keyPressed == "RightButton" then
-			keyPressed = "BUTTON2"
-		elseif keyPressed == "MiddleButton" then
-			keyPressed = "BUTTON3"
-		elseif keyPressed == "Button4" then
-			keyPressed = "BUTTON4"
-		elseif keyOrButton == "Button5" then
-			keyPressed = "BUTTON5"
-		elseif keyPressed == "Button6" then
-			keyPressed = "BUTTON6"
-		elseif keyOrButton == "Button7" then
-			keyPressed = "BUTTON7"
-		elseif keyPressed == "Button8" then
-			keyPressed = "BUTTON8"
-		elseif keyOrButton == "Button9" then
-			keyPressed = "BUTTON9"
-		elseif keyPressed == "Button10" then
-			keyPressed = "BUTTON10"
-		elseif keyOrButton == "Button11" then
-			keyPressed = "BUTTON11"
-		elseif keyPressed == "Button12" then
-			keyPressed = "BUTTON12"
-		elseif keyOrButton == "Button13" then
-			keyPressed = "BUTTON13"
-		elseif keyPressed == "Button14" then
-			keyPressed = "BUTTON14"
-		elseif keyOrButton == "Button15" then
-			keyPressed = "BUTTON15"
-		elseif keyPressed == "Button16" then
-			keyPressed = "BUTTON16"
-		elseif keyOrButton == "Button17" then
-			keyPressed = "BUTTON17"
-		elseif keyPressed == "Button18" then
-			keyPressed = "BUTTON18"
-		elseif keyOrButton == "Button19" then
-			keyPressed = "BUTTON19"
-		elseif keyPressed == "Button20" then
-			keyPressed = "BUTTON20"
-		elseif keyOrButton == "Button21" then
-			keyPressed = "BUTTON21"
-		elseif keyPressed == "Button22" then
-			keyPressed = "BUTTON22"
-		elseif keyOrButton == "Button23" then
-			keyPressed = "BUTTON23"
-		elseif keyPressed == "Button24" then
-			keyPressed = "BUTTON24"
-		elseif keyOrButton == "Button25" then
-			keyPressed = "BUTTON25"
-		elseif keyPressed == "Button26" then
-			keyPressed = "BUTTON26"
-		elseif keyOrButton == "Button27" then
-			keyPressed = "BUTTON27"
-		elseif keyPressed == "Button28" then
-			keyPressed = "BUTTON28"
-		elseif keyOrButton == "Button29" then
-			keyPressed = "BUTTON29"
-		elseif keyPressed == "Button30" then
-			keyPressed = "BUTTON30"
-		elseif keyOrButton == "Button31" then
-			keyPressed = "BUTTON31"
-		end
-		if keyPressed == "BUTTON1" or keyPressed == "BUTTON2" then
-			return
-		end
+		if not IsKeyPressIgnoredForBinding(keyPressed) then
+			keyPressed = CreateKeyChordString(keyPressed)
 
-		if keyPressed == "LSHIFT" or
-			keyPressed == "RSHIFT" or
-			keyPressed == "LCTRL" or
-			keyPressed == "RCTRL" or
-			keyPressed == "LALT" or
-			keyPressed == "RALT" then
-			return
+			self:setBinding(keyPressed, self.selected.command)
+			self:setButtonText(self.selected)
+			self:setSelected()
 		end
-		if IsShiftKeyDown() then
-			keyPressed = "SHIFT-"..keyPressed
-		end
-		if IsControlKeyDown() then
-			keyPressed = "CTRL-"..keyPressed
-		end
-		if IsAltKeyDown() then
-			keyPressed = "ALT-"..keyPressed
-		end
-
-		self:setBinding(keyPressed, self.selected.command)
-		self:setButtonText(self.selected)
-		self:setSelected()
 	end
 end
 binding:SetScript("OnKeyDown", binding.OnKeyDown)
