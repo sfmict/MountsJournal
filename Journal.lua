71cc74d649a0d3802a5de79bfae35b6f38cddeef
sfmict <sfmict@gmail.com>
2020-09-16 23:28:17 +0300

    camera update

--- Journal.lua
+++ Journal.lua
@@ -629,102 +629,7 @@ function journal:ADDON_LOADED(addonName)
 		modelControl.rotateDownButton.icon:SetRotation(-math.pi / 1.6)
 
 		hooksecurefunc(self.modelScene, "SetActiveCamera", function(self)
-			local activeCamera = self.activeCamera
-			activeCamera.panningXOffset = 0
-			activeCamera.panningYOffset = 0
-
-			local function TryCreateZoomSpline(x, y, z, existingSpline)
-				if x and y and z and (x ~= 0 or y ~= 0 or z ~= 0) then
-					local spline = existingSpline or CreateCatmullRomSpline(3)
-					spline:ClearPoints()
-					spline:AddPoint(0, 0, 0)
-					spline:AddPoint(x, y, z)
-
-					return spline
-				end
-			end
-
-			function activeCamera:ApplyFromModelSceneCameraInfo(modelSceneCameraInfo, transitionType, modificationType) -- override
-				modelSceneCameraInfo.target.z = 1
-				modelSceneCameraInfo.maxZoomDistance = 24
-				if modificationType ~= CAMERA_MODIFICATION_TYPE_MAINTAIN then
-					self.xOffset = 0
-					self.yOffset = mountDescriptionToggle:GetChecked() and 40 or 0
-				end
-
-				local transitionalCameraInfo = self:CalculateTransitionalValues(self.modelSceneCameraInfo, modelSceneCameraInfo, modificationType)
-
-				self.modelSceneCameraInfo = modelSceneCameraInfo
-
-				self:SetTarget(transitionalCameraInfo.target:GetXYZ())
-				self:SetTargetSpline(TryCreateZoomSpline(transitionalCameraInfo.zoomedTargetOffset:GetXYZ()), self:GetTargetSpline())
-				self:SetOrientationSpline(TryCreateZoomSpline(transitionalCameraInfo.zoomedYawOffset, transitionalCameraInfo.zoomedPitchOffset, transitionalCameraInfo.zoomedRollOffset), self:GetOrientationSpline())
-
-				self:SetMinZoomDistance(transitionalCameraInfo.minZoomDistance)
-				self:SetMaxZoomDistance(transitionalCameraInfo.maxZoomDistance)
-
-				self:SetZoomDistance(transitionalCameraInfo.zoomDistance)
-
-				self:SetYaw(transitionalCameraInfo.yaw)
-				self:SetPitch(transitionalCameraInfo.pitch)
-				self:SetRoll(transitionalCameraInfo.roll)
-
-				if transitionType == CAMERA_TRANSITION_TYPE_IMMEDIATE then
-					self:SnapAllInterpolatedValues()
-				end
-				self:UpdateCameraOrientationAndPosition()
-
-				self:SaveInitialTransform(transitionalCameraInfo)
-			end
-
-			local function InterpolateDimension(lastValue, targetValue, amount, elapsed)
-				return lastValue and amount and DeltaLerp(lastValue, targetValue, amount, elapsed) or targetValue
-			end
-
-			hooksecurefunc(activeCamera, "UpdateInterpolationTargets", function(self, elapsed)
-				self.panningXOffset = InterpolateDimension(self.panningXOffset, self.xOffset, .15, elapsed)
-				self.panningYOffset = InterpolateDimension(self.panningYOffset, self.yOffset, .15, elapsed)
-			end)
-
-			local HandleMouseMovement = activeCamera.HandleMouseMovement
-			function activeCamera:HandleMouseMovement(mode, delta, snapToValue)
-				if mode == ORBIT_CAMERA_MOUSE_PAN_HORIZONTAL then
-					self.xOffset = self.xOffset + delta
-
-					if snapToValue then
-						self.panningXOffset = self.xOffset
-					end
-				elseif mode == ORBIT_CAMERA_MOUSE_PAN_VERTICAL then
-					self.yOffset = self.yOffset + delta
-
-					if snapToValue then
-						self.panningYOffset = self.yOffset
-					end
-				else
-					HandleMouseMovement(self, mode, delta, snapToValue)
-				end
-			end
-
-			activeCamera:SetLeftMouseButtonYMode(ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION, true)
-			activeCamera:SetRightMouseButtonXMode(ORBIT_CAMERA_MOUSE_PAN_HORIZONTAL, true)
-			activeCamera:SetRightMouseButtonYMode(ORBIT_CAMERA_MOUSE_PAN_VERTICAL, true)
-
-			activeCamera.deltaModifierForCameraMode = {
-				[ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_YAW_ROTATION),
-				[ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION] = -activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_PITCH_ROTATION),
-				[ORBIT_CAMERA_MOUSE_MODE_ROLL_ROTATION] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_ROLL_ROTATION),
-				[ORBIT_CAMERA_MOUSE_MODE_ZOOM] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_ZOOM),
-				[ORBIT_CAMERA_MOUSE_MODE_TARGET_HORIZONTAL] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_TARGET_HORIZONTAL),
-				[ORBIT_CAMERA_MOUSE_MODE_TARGET_VERTICAL] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_MODE_TARGET_VERTICAL),
-				[ORBIT_CAMERA_MOUSE_PAN_HORIZONTAL] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_PAN_HORIZONTAL),
-				[ORBIT_CAMERA_MOUSE_PAN_VERTICAL] = activeCamera:GetDeltaModifierForCameraMode(ORBIT_CAMERA_MOUSE_PAN_VERTICAL),
-			}
-			setmetatable(activeCamera.deltaModifierForCameraMode, {__index = function()
-				return 0
-			end})
-			function activeCamera:GetDeltaModifierForCameraMode(mode)
-				return self.deltaModifierForCameraMode[mode]
-			end
+			journal:event("SET_ACTIVE_CAMERA", self.activeCamera)
 		end)
 
 		modelControl.panButton:HookScript("OnMouseDown", function(self)
@@ -761,11 +666,6 @@ function journal:ADDON_LOADED(addonName)
 
 		modelControl.reset:SetScript("OnClick", function(self)
 			local activeCamera = self:GetParent():GetParent().activeCamera
-			local pi2 = math.pi * 2
-			activeCamera.yaw = math.fmod(activeCamera.yaw, pi2)
-			activeCamera.interpolatedYaw = math.fmod(activeCamera.interpolatedYaw, pi2)
-			activeCamera.pitch = math.fmod(activeCamera.pitch, pi2)
-			activeCamera.interpolatedPitch = math.fmod(activeCamera.interpolatedPitch, pi2)
 			activeCamera:ApplyFromModelSceneCameraInfo(activeCamera.modelSceneCameraInfo)
 		end)
 
