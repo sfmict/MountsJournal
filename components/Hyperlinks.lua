1c58eb0295a681484d5fb14afd63186ed04bb8f4
sfmict <sfmict@gmail.com>
2022-07-18 10:36:58 +0300

    Fixed opening links in combat.

--- components/Hyperlinks.lua
+++ components/Hyperlinks.lua
@@ -1,7 +1,7 @@
 hooksecurefunc("HandleModifiedItemClick", function(link)
 	local config = MountsJournal.config
 
-	if config.openHyperlinks and not config.useDefaultJournal and IsModifiedClick("DRESSUP") and not IsModifiedClick("CHATLINK") then
+	if config.openHyperlinks and not config.useDefaultJournal and not InCombatLockdown() and IsModifiedClick("DRESSUP") and not IsModifiedClick("CHATLINK") then
 		local _,_,_, linkType, linkID = (":|H"):split(link)
 
 		local mountID
@@ -12,15 +12,12 @@ hooksecurefunc("HandleModifiedItemClick", function(link)
 		end
 
 		if mountID then
-			local isCollectionsLoaded = IsAddOnLoaded("Blizzard_Collections")
-			if isCollectionsLoaded or not InCombatLockdown() then
-				HideUIPanel(DressUpFrame)
-				if not isCollectionsLoaded then
-					LoadAddOn("Blizzard_Collections")
-				end
-				ShowUIPanel(CollectionsJournal)
-				MountsJournalFrame:setSelectedMount(mountID)
+			HideUIPanel(DressUpFrame)
+			if not IsAddOnLoaded("Blizzard_Collections") then
+				LoadAddOn("Blizzard_Collections")
 			end
+			ShowUIPanel(CollectionsJournal)
+			MountsJournalFrame:setSelectedMount(mountID)
 		end
 	end
 end)
